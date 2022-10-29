295f3713c9b14727288c6188e29fd6606f312d4a
Aniruddha Mandal <aniruddha1.2.3.3.2.1@gmail.com>
2022-10-29 10:16:10 +0530

    Updated homepage

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -1,19 +1,23 @@
 import streamlit as st
 import requests
+from PIL import Image
 
 st.set_page_config(
     page_title="Namaste",
     page_icon="🙏",
 )
 
+image = Image.open('cover.jpg')
 
 st.write("# Welcome to Bank Note Authentication System!")
+
+st.image(image)
 
 st.sidebar.success("Select a page.")
 
 st.markdown(
     """
-    
+        An ML classifier web app to classify bank notes whether they are fake or not
     """
     )
 
